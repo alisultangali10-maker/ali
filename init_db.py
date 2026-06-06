5b99c195fa98659c7b98a2a3e370a6eeb9cad642
alisultangali10-maker <alisultangali10@gmail.com>
2026-06-06 12:37:31 +0500

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -9,8 +9,8 @@ with app.app_context():
     # Create default categories if none
     if Category.query.count() == 0:
         categories = [
-            'cat_general', 'cat_politics', 'cat_sport', 
-            'cat_economy', 'cat_tech', 'cat_world', 
+            'cat_general', 'cat_politics', 'cat_sport',
+            'cat_economy', 'cat_tech', 'cat_world',
             'cat_auto', 'cat_culture'
         ]
         for c in categories:
@@ -20,11 +20,15 @@ with app.app_context():
         print("Категории успешно добавлены.")
 
     # Create default admin user
-    if User.query.filter_by(username='admin').first() is None:
-        admin_user = User(username='admin')
-        admin_user.set_password('admin')
-        db.session.add(admin_user)
+    admin = User.query.filter_by(username='admin').first()
+
+    if not admin:
+        admin = User(username='admin')
+        admin.set_password('admin')
+        db.session.add(admin)
         db.session.commit()
-        print("Пользователь admin создан с паролем admin.")
+        print("ADMIN CREATED (admin/admin)")
+    else:
+        print("ADMIN ALREADY EXISTS")
 
     print("База данных инициализирована.")
