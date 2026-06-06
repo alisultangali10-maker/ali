37438fa6227c6804d4a4eb8e2e3225a48326c1a2
alisultangali10-maker <alisultangali10@gmail.com>
2026-06-06 14:01:47 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,24 +1,6 @@
-from app import create_app, db
-from app.models import User, Category
+from app import create_app
 
 app = create_app()
-
-# 🔥 ДОБАВИТЬ ЭТО БЛОК
-with app.app_context():
-    db.create_all()
-
-    # create admin if not exists
-    admin = User.query.filter_by(username='admin').first()
-
-    if not admin:
-        admin = User(username='admin')
-        admin.set_password('admin')
-        db.session.add(admin)
-        db.session.commit()
-        print("ADMIN CREATED")
-
-# 🔥 END INIT BLOCK
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
