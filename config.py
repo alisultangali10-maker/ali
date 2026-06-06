9f02064fa833ece37d2505bf84031f9c0638aed3
alisultangali10-maker <alisultangali10@gmail.com>
2026-06-06 11:56:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///news_kz.db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
